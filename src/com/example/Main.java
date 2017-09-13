51bb41f1d620e468309378556787a09b9f8e6dfd
Brian Sheen <bsheen@gmail.com>
2017-09-13 16:39:53 -0500

    solved kinda

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,25 +3,35 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-        String s = "aabbaddba";
+        String s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
         System.out.println(longestPalindrome(s));
     }
 
     public static String longestPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
         StringBuilder reverse = new StringBuilder();
-        int count = 0;
+        int longest = 0;
+        int count;
+        int firstIndex = 0;
         for(int i = 0; i < s.length(); ++i){
-            System.out.println("char at " + i + ": " + s.charAt(i));
-            sb.append(s.charAt(i));
-            System.out.println("sb= " + sb);
-            reverse.append(sb.toString());
-            if(sb.toString().equals(reverse.reverse().toString())){
-                System.out.println("sbreverse= " +reverse);
-                count = reverse.length();
+            for(int j = i; j < s.length(); ++j){
+                System.out.println("char at " + j + ": " + s.charAt(j));
+                sb.append(s.charAt(j));
+                System.out.println("sb= " + sb);
+                reverse.append(sb.toString());
+                if(sb.toString().equals(reverse.reverse().toString())){
+                    System.out.println("sbreverse= " +reverse);
+                    count = reverse.length();
+                    if(count > longest){
+                        longest = count;
+                        firstIndex = i;
+                    }
+                }
+                reverse.delete(0,reverse.length());
             }
-            reverse.delete(0,reverse.length());
+            sb.delete(0,sb.length());
         }
-        return String.valueOf(count);
+        sb.append(s);
+        return sb.substring(firstIndex,firstIndex+longest);
     }
 }
